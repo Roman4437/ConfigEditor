f3744bfa39fd109b0b1735fdcbb112217cc98de5
Roman4437 <romanbabalici97@gmail.com>
2023-04-12 15:32:44 +0200

    added pagination

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { useParser } from '../hooks/useParser';
 
 import Row from './components/Row';
 
+const ITEMS_ON_PAGE = 14
+
 function App() {
   const { parsedData, parse } = useParser()
   const [data, setData] = useState<string>()
+  const [rows, setRows] = useState<{item: string, order: number}[]>()
+  const [activePage, setActivePage] = useState<number>(0)
 
   const fileInputRef = useRef<HTMLInputElement>(null)
-  const mainBlockRef = useRef<HTMLDivElement>(null)
 
   const handleOpenFilePicker = () => fileInputRef.current?.click()
 
@@ -16,7 +19,7 @@ function App() {
     const updateData = data?.split("\n")
     updateData?.push('NewItem, 0, NewItem, 0')
 
-    setData(updateData?.join('\n')) 
+    setData(updateData?.join('\n'))
   }
 
   const handleAddToClipboard = () => data && navigator.clipboard.writeText(data)
@@ -26,24 +29,35 @@ function App() {
   }, [parsedData]);
 
   useEffect(() => {
-    mainBlockRef.current?.scrollTo(0, mainBlockRef.current?.scrollHeight)
+    const rows: {item: string, order: number}[] = []
+
+    data?.split('\n').map((item, order) => 
+      item !== '\r' && !item.includes('[') && !item.includes('#') && rows.push({ item,  order }))
+
+    setRows(rows)
+
+    setActivePage(Math.ceil(rows.length / ITEMS_ON_PAGE) - 1)
   }, [data])
 
   return (
     <div className='flex flex-col gap-y-4 p-4 h-screen items-center text-white bg-[#0d1017] overflow-hidden'>
       <input ref={fileInputRef} type="file" onChange={parse} hidden/>
       <div className='flex w-3/4 justify-between'>
-        <button onClick={handleOpenFilePicker} className='py-2 px-4 bg-blue-400 text-xl'>Open</button>
+        <button onClick={handleOpenFilePicker} className='py-2 px-4 hover:bg-blue-300 bg-blue-400 text-xl transition-colors ease-in-out'>Open</button>
         <div className='flex gap-4'>
-          <button onClick={handleAddNewRow} className='py-2 px-4 bg-purple-400 text-xl'>New Row</button>
-          <button onClick={handleAddToClipboard} className='py-2 px-4 bg-red-400 text-xl'>Copy</button>
+          <button onClick={handleAddNewRow} className='py-2 px-4 hover:bg-purple-300 bg-purple-400 text-xl transition-colors ease-in-out'>New Row</button>
+          <button onClick={handleAddToClipboard} className='py-2 px-4 hover:bg-red-300 bg-red-400 text-xl transition-colors ease-in-out'>Copy</button>
         </div>
       </div>
-      <div ref={mainBlockRef} className='w-3/4 overflow-y-scroll scrollbar-hide'>
+      <div className='w-3/4 overflow-y-scroll scrollbar-hide'>
         {data 
-          ? data.split('\n').map((item, index) => 
-            item.includes(' ') && !item.includes('[') && !item.includes('#') && <Row key={item + index} data={data} order={index} mutate={setData}>{item}</Row>)
+          ? rows?.slice(activePage * ITEMS_ON_PAGE, activePage * ITEMS_ON_PAGE + ITEMS_ON_PAGE).map(({item, order}) => 
+            <Row key={crypto.randomUUID()} data={data} order={order} mutate={setData}>{item}</Row>)
           : <p className='text-white text-center'>None.</p>}
+      </div>
+      <div className='fixed bottom-10 flex gap-2 flex-wrap'>
+        {rows && Array.from({ length: Math.ceil(rows.length / ITEMS_ON_PAGE) }, (item, page) => 
+          <p onClick={() => setActivePage(page)} className={`flex cursor-pointer items-center justify-center h-6 w-6 text-xs border hover:bg-blue-400 border-white ${activePage === page && 'bg-blue-500'}`}>{page + 1}</p>)}
       </div>
     </div>
   );
--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -7,7 +7,7 @@ interface props {
   mutate: React.Dispatch<React.SetStateAction<string | undefined>>
 }
 
-export default function Row({children, data, order, mutate}: props) {
+export default function Row({ children, data, order, mutate }: props) {
   const [first, setFirst] = useState(children.split(',')[0].trim())
   const [second, setSecond] = useState(children.split(',')[1].trim())
   const [third, setThird] = useState(children.split(',')[2].trim())
@@ -35,8 +35,8 @@ export default function Row({children, data, order, mutate}: props) {
       <input type="text" value={second} onChange={event => setSecond(event.currentTarget.value)} className="bg-transparent w-1/4 p-2 overflow-hidden border border-[#343434] outline-none"/>
       <input type="text" value={third} onChange={event => setThird(event.currentTarget.value)} className="bg-transparent w-1/4 p-2 overflow-hidden border border-[#343434] outline-none"/>
       <div className="flex w-1/4 p-2 overflow-hidden justify-between items-center border border-[#343434] outline-none">
-        <input type="text" className="bg-transparent outline-none" value={fourth} onChange={event => setFourth(event.currentTarget.value)}/>
-        <div onClick={handleRemoveRow} className="flex justify-center items-center h-4 w-4 bg-red-600 text-xs border border-black cursor-pointer">x</div>
+        <input type="text" value={fourth} onChange={event => setFourth(event.currentTarget.value)} className="bg-transparent outline-none"/>
+        <div onClick={handleRemoveRow} className="flex justify-center items-center h-5 w-5 hover:bg-red-400 bg-red-600 text-xs border border-white cursor-pointer">&#10005;</div>
       </div>
       <button type="submit"/>
     </form>
